80d288bbc3120acaeb85817588ffb8463c234161
Sourabh <sppathak1428@gmail.com>
2024-09-11 21:54:16 +0530

    Da

--- trackPixel.go
+++ trackPixel.go
@@ -6,19 +6,18 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 )
 
 func trackHandler(w http.ResponseWriter, r *http.Request) {
 	email := r.URL.Query().Get("email")
-	log.Printf("Email opened by: %s at %s\n", email, time.Now().Format(time.RFC3339))
+	log.Printf("Email opened by: %s\n", email)
 
+	// Return a 1x1 transparent GIF
 	pixel, err := ioutil.ReadFile("transparent.gif")
 	if err != nil {
 		http.Error(w, "Error loading pixel", http.StatusInternalServerError)
 		return
 	}
-
 	w.Header().Set("Content-Type", "image/gif")
 	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(pixel)))
 	w.Write(pixel)
@@ -30,9 +29,15 @@ func main() {
 	// Get the port from the environment variable
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "10000" // Default to port 10000 if PORT is not set
+		port = "10000" // Default port
 	}
 
+	// Log the start
 	log.Printf("Server starting on port %s...\n", port)
-	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, nil)) // Bind to 0.0.0.0 to listen on all interfaces
+
+	// Start server on 0.0.0.0 and the port from the environment
+	err := http.ListenAndServe("0.0.0.0:"+port, nil)
+	if err != nil {
+		log.Fatal("Server failed to start:", err)
+	}
 }
