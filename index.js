dcab7f0047f68ae6a4cdaca3b325a051d83a1dfa
Sourabh <sppathak1428@gmail.com>
2024-09-14 02:00:24 +0530

    Dsa

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/pue', (req, res) => {
   res.send('piduuuu!!!!!!!!!!');
 });
 app.get('/jue', (req, res) => {
-  res.send('hahahahahah!!!!!!!!!!');
+  res.send('chummmii------------------------');
 });
 
 // Sample POST route
