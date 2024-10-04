1e7e983a39259a7aeebb3498f39215b41633816e
Sourabh <sppathak1428@gmail.com>
2024-10-04 13:43:05 +0530

    Da

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
         script: './index.js',
         env: {
           JWT_SECRET: process.env.JWT_SECRET || 'default_jwt_secret',
-          MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase',
+          MONGODB_URI: "mongodb+srv://sppathak1428:1vmAkV1LIypO4bVQ@cluster0.hldmans.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" || 'mongodb://localhost:27017/mydatabase',
           NAME: process.env.NAME || 'default_name',
           PORT: process.env.PORT || 3000,
           REDIS_PASSWORD: process.env.REDIS_PASSWORD || 'default_redis_password',
