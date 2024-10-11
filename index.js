05ffe5c59ef6b2ef8df0b928c4c57d9d67eff556
Sourabh <sppathak1428@gmail.com>
2024-10-12 03:17:16 +0530

    Dass

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ router.use(apiLimiter);
 const routes = require('./routes/users.js');
 const email = require('./routes/Email.js');
 const eventRoutes=require('./routes/events.js');
-
+const users=require('./routes/users.js');
 const campaignRoutes = require('./routes/campaign.js');
 const { validate } = require('./Modal.js');
 
@@ -63,6 +63,7 @@ app.use('/events',validateApiKey, eventRoutes);
 app.use('/campaigns',validateApiKey ,campaignRoutes);
 app.use('/auth',validateApiKey,authRoute );
 app.use('/email',validateApiKey, email);
+app.use('/users',validateApiKey, users);
 
 app.use('/keep-alive',(req,res)=>{
   res.json({"data":"Main server is Alive"});
