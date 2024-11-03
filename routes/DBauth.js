2e918af4561b11f9964eb70015272f0dbfc191e4
Sourabh <sppathak1428@gmail.com>
2024-11-03 22:31:35 +0530

    Das

--- routes/DBauth.js
+++ routes/DBauth.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 // Replace with your MongoDB connection URI
 
 const uri = process.env.MONGODB_URI;
-// Connect to MongoDB
+// Connect to MongoDBs
 
 const client = new MongoClient(uri);
 client.connect();
